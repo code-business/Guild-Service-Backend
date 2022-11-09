e6985ad1272f4023179de1a93517bdb97aef9467
Rahul Patil <rahulrohidaspatil@gmail.com>
2022-11-09 15:19:14 +0530

    guilds module & service updated

--- src/guilds/guilds.module.ts
+++ src/guilds/guilds.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { GuildsService } from './guilds.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { GuildsController } from './guilds.controller';
+import { GuildsService } from './guilds.service';
+import { Guild, GuildSchema } from './schemas/guild.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Guild.name, schema: GuildSchema }]),
+  ],
   controllers: [GuildsController],
-  providers: [GuildsService]
+  providers: [GuildsService],
 })
 export class GuildsModule {}
--- src/guilds/guilds.service.ts
+++ src/guilds/guilds.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateGuildDto } from './dto/create-guild.dto';
 import { UpdateGuildDto } from './dto/update-guild.dto';
+import { Guild, GuildDocument } from './schemas/guild.schema';
 
 @Injectable()
 export class GuildsService {
+  constructor(
+    @InjectModel(Guild.name) private guildModel: Model<GuildDocument>,
+  ) {}
+
   create(createGuildDto: CreateGuildDto) {
-    return 'This action adds a new guild';
+    const createdGuild = new this.guildModel(createGuildDto);
+    return createdGuild.save();
+    // return 'This action adds a new guild';
   }
 
   findAll() {
-    return `This action returns all guilds`;
+    return this.guildModel.find().exec();
+    // return `This action returns all guilds`;
   }
 
   findOne(id: number) {
