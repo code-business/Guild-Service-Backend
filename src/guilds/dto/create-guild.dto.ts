c7e2dce050a9688678e8102986f7c71cdae733e3
Rahul Patil <rahulrohidaspatil@gmail.com>
2022-11-09 18:42:41 +0530

    swagger api properties updated

--- src/guilds/dto/create-guild.dto.ts
+++ src/guilds/dto/create-guild.dto.ts
@@ -1,9 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateGuildDto {
     @IsNotEmpty()
+    @ApiProperty({
+        default: 'Rahul Patil',
+    })
     name: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        default: 24
+    })
     age: string;
 }
