82db1b702b4c4fbde86c9673e56533744c602363
navnath <navnath.parte@codeb.online>
2022-12-05 14:56:24 +0530

    error handle

--- src/guilds/guilds.controller.ts
+++ src/guilds/guilds.controller.ts
@@ -2,7 +2,6 @@ import { Metadata } from '@metaplex-foundation/js';
 import { HttpService } from '@nestjs/axios';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Transaction } from '@solana/web3.js';
 import {
   BadgeRecordStatusEnum,
   BadgeTypeEnum,
@@ -24,65 +23,89 @@ export class GuildsController {
 
   @Get('getBadges/:publicKey')
   async getBadges(@Param('publicKey') publicKey: string) {
-    const badgeRecords = await this.guildsService.getBadgeRecords(publicKey);
-    if (!badgeRecords.length) {
-      const myNfts = await this.guildsService.getMyBadges(publicKey);
-      for (const metadata of myNfts) {
-        const { mintAddress, name, symbol } = metadata as Metadata;
-        if (
-          symbol === 'GARI' &&
-          (name === 'Iron Creator' || name === 'Iron User')
-        ) {
-          const doc = {
-            publicKey,
-            mintAddress,
-            badgeType:
-              name === 'Iron Creator'
-                ? BadgeTypeEnum.CREATOR
-                : BadgeTypeEnum.USER,
-            status: BadgeRecordStatusEnum.SUCCESS,
-          };
-          const newBadgeRecord = await this.guildsService.addBadgeRecord(doc);
-          badgeRecords.push(newBadgeRecord);
+    try {
+      const badgeRecords = await this.guildsService.getBadgeRecords(publicKey);
+      if (!badgeRecords.length) {
+        const myNfts = await this.guildsService.getMyBadges(publicKey);
+        for (const metadata of myNfts) {
+          const { mintAddress, name, symbol } = metadata as Metadata;
+          if (
+            symbol === 'GARI' &&
+            (name === 'Iron Creator' || name === 'Iron User')
+          ) {
+            const badgeRecord = {
+              publicKey,
+              mintAddress,
+              badgeType:
+                name === 'Iron Creator'
+                  ? BadgeTypeEnum.CREATOR
+                  : BadgeTypeEnum.USER,
+              status: BadgeRecordStatusEnum.SUCCESS,
+            };
+            const newBadgeRecord = await this.guildsService.addBadgeRecord(
+              badgeRecord,
+            );
+            badgeRecords.push(newBadgeRecord);
+          }
         }
       }
+      return badgeRecords;
+    } catch (error) {
+      return {
+        code: 400,
+        message: 'Error',
+        error: error.message,
+      };
     }
-    return badgeRecords;
   }
 
   @Post('buyBadge')
   async buyBadge(@Body() body: BuyBadgeDto) {
-    const { publicKey, badgeType } = body;
-    const response = await this.httpService.axiosRef.post(
-      `${constant.NFT_SERVICE_URL}/nft/V2/create`,
-      {
-        usdToGari: 1,
-        userPublicKey: publicKey,
-        badge: '2x',
-        type: badgeType,
-        feePayer: publicKey,
-      },
-    );
-    const rawTransaction = response.data.transaction;
-    const buffer = Buffer.from(rawTransaction, 'base64');
-    const decodedTransaction = Transaction.from(buffer);
-    const mintAddress = decodedTransaction.signatures[1].publicKey.toString();
-    const badgeRecord = await this.guildsService.addDraftBadgeRecord(
-      publicKey,
-      badgeType,
-      mintAddress,
-    );
-    return { badgeRecord, rawTransaction };
+    try {
+      const { publicKey, badgeType } = body;
+      const response = await this.httpService.axiosRef.post(
+        `${constant.NFT_SERVICE_URL}/nft/V2/create`,
+        {
+          usdToGari: 1,
+          userPublicKey: publicKey,
+          badge: '2x',
+          type: badgeType,
+          feePayer: publicKey,
+        },
+      );
+      const rawTransaction = response.data.transaction;
+      const mintAddress = this.guildsService.getMintAddress(rawTransaction);
+      const badgeRecord = await this.guildsService.addDraftBadgeRecord(
+        publicKey,
+        badgeType,
+        mintAddress,
+      );
+      return { badgeRecord, rawTransaction };
+    } catch (error) {
+      return {
+        code: 400,
+        message: 'Error',
+        error: error.message,
+      };
+    }
   }
 
   @Post('updateBadgeRecord')
-  updateBadgeRecord(@Body() body: UpdateBadgeRecordDto) {
-    const { publicKey, badgeType, mintAddress } = body;
-    return this.guildsService.updateBadgeRecord(
-      publicKey,
-      badgeType,
-      mintAddress,
-    );
+  async updateBadgeRecord(@Body() body: UpdateBadgeRecordDto) {
+    try {
+      const { publicKey, badgeType, mintAddress } = body;
+      const response = await this.guildsService.updateBadgeRecord(
+        publicKey,
+        badgeType,
+        mintAddress,
+      );
+    } catch (error) {
+      return {
+        code: 400,
+        message: 'Error',
+        error: error.message,
+      };
+    }
   }
 
   @Post('raiseBadgeRequest')
